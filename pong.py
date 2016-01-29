3174a86a458988b49555772ed8b9dd31f331228c
NikolayP <kolya-paskalov@mail.ru>
2016-01-29 18:23:42 +0300

    LENIN

--- pong.py
+++ pong.py
@@ -26,18 +26,18 @@ done = False
     # Игроков
 
 # 1 Игрок(Plr1)
-widthPlatfPlr1 = 60
-heightPlatfPlr1 = 12
+widthPlatfPlr1 = 12
+heightPlatfPlr1 = 60
 speedPlatfPlr1 = 2
-coordXPlatfPlr1 = (winWidth-widthPlatfPlr1)/2
+coordYPlatfPlr1 = (winHeight-heightPlatfPlr1)/2
 platfPlr1L = False
 platfPlr1R = False
 
 # 2 Игрок(Plr2)
-widthPlatfPlr2 = 60
-heightPlatfPlr2 = 12
+widthPlatfPlr2 = 12
+heightPlatfPlr2 = 60
 speedPlatfPlr2 = 2
-coordXPlatfPlr2 = (winWidth-widthPlatfPlr2)/2
+coordYPlatfPlr2 = (winHeight-heightPlatfPlr2)/2
 platfPlr2L = False
 platfPlr2R = False
     # Шарика
@@ -79,29 +79,29 @@ while not done:
         
     # Условие движение платформы Игрока 1
     if platfPlr1L == True:
-        coordXPlatfPlr1-=speedPlatfPlr1
-        # Обработка левой границы 
-        if coordXPlatfPlr1 < 0:
-            coordXPlatfPlr1 = 0
+        coordYPlatfPlr1 -= speedPlatfPlr1
+        # Обработка верхней границы 
+        if coordYPlatfPlr1 < 0:
+            coordYPlatfPlr1 = 0
             
     if platfPlr1R == True:
-        coordXPlatfPlr1+=speedPlatfPlr1
-        # Обработка правой границы
-        if coordXPlatfPlr1 > winWidth-widthPlatfPlr1:
-            coordXPlatfPlr1 = winWidth-widthPlatfPlr1
+        coordYPlatfPlr1 += speedPlatfPlr1
+        # Обработка нижней границы
+        if coordYPlatfPlr1 > winHeight-heightPlatfPlr1:
+            coordYPlatfPlr1 = winHeight-heightPlatfPlr1
             
     # Условие движение платформы Игрока 2
     if platfPlr2L == True:
-        coordXPlatfPlr2-=speedPlatfPlr2
+        coordYPlatfPlr2-=speedPlatfPlr2
         # Обработка левой границы 
-        if coordXPlatfPlr2 < 0:
-            coordXPlatfPlr2 = 0
+        if coordYPlatfPlr2 < 0:
+            coordYPlatfPlr2 = 0
                         
     if platfPlr2R == True:
-        coordXPlatfPlr2+=speedPlatfPlr2
+        coordYPlatfPlr2+=speedPlatfPlr2
         # Обработка правой границы
-        if coordXPlatfPlr2 > winWidth-widthPlatfPlr2:
-            coordXPlatfPlr2 = winWidth-widthPlatfPlr2
+        if coordYPlatfPlr2 > winHeight-heightPlatfPlr2:
+            coordYPlatfPlr2 = winHeight-heightPlatfPlr2
             
         # Пересечение шарика и платформы
         
@@ -118,10 +118,10 @@ while not done:
     
         # Основной режим игры
     # Прорисовка платформы игрока 1
-    pygame.draw.rect(screen, black, [coordXPlatfPlr1, winHeight - (heightPlatfPlr1 + 2), widthPlatfPlr1, heightPlatfPlr1])
+    pygame.draw.rect(screen, black, [2, coordYPlatfPlr1, widthPlatfPlr1, heightPlatfPlr1])
     
     # Прорисовка платформы игрока 2
-    pygame.draw.rect(screen, black, [coordXPlatfPlr2, 2, widthPlatfPlr2, heightPlatfPlr2])
+    pygame.draw.rect(screen, black, [winWidth-(widthPlatfPlr1+2), coordYPlatfPlr2, widthPlatfPlr2, heightPlatfPlr2])
         # Конец игры
 
     pygame.display.update()      
