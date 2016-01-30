b22649a48a7ccaca08956d39455554cc813587a6
NikolayP <kolya-paskalov@mail.ru>
2016-01-30 22:59:24 +0300

    Realizoval Game Over Daniil i Nikolay - teper delo za vami

--- pong.py
+++ pong.py
@@ -22,6 +22,7 @@ pygame.display.set_caption("Ping Pong")
 clock = pygame.time.Clock()
 done = False
 
+
 # Инициализация
     # Игроков
 
@@ -42,14 +43,20 @@ coordYPlatfPlr2 = (winHeight - heightPlatfPlr2)/2
 platfPlr2U = False
 platfPlr2D = False
 scorePlr2 = 0
+
     # Шарика
 coordXBall = winWidth//2
 coordYBall = winHeight//2
 radiusBall = 7
 speedXBall = 1 
-speedYBall = 1   
-    
+speedYBall = 1
+
+    # Конца Игры
+gameOverPlr1 = False
+gameOverPlr2 = False
+
     # Бонусов
+
 
 # Основной цикл
 while not done:
@@ -80,77 +87,95 @@ while not done:
             if event.key == pygame.K_a:
                 platfPlr2U = False
             if event.key == pygame.K_d:
-                platfPlr2D = False                
+                platfPlr2D = False 
+            
     # Игровая логика
-     
         # Обработка игроков
-    # Условие движение платформы Игрока 1
+            # Условие движение платформы Игрока 1
     if platfPlr1U == True:
         coordYPlatfPlr1 -= speedPlatfPlr1
-        # Обработка верхней границы 
+            # Обработка верхней границы 
         if coordYPlatfPlr1 < 0:
             coordYPlatfPlr1 = 0
             
     if platfPlr1D == True:
         coordYPlatfPlr1 += speedPlatfPlr1
-        # Обработка нижней границы
+            # Обработка нижней границы
         if coordYPlatfPlr1 > winHeight - heightPlatfPlr1:
             coordYPlatfPlr1 = winHeight - heightPlatfPlr1
             
-    # Условие движение платформы Игрока 2
+            # Условие движение платформы Игрока 2
     if platfPlr2U == True:
         coordYPlatfPlr2 -= speedPlatfPlr2
-        # Обработка верхней границы 
+            # Обработка верхней границы 
         if coordYPlatfPlr2 < 0:
             coordYPlatfPlr2 = 0
                         
     if platfPlr2D == True:
         coordYPlatfPlr2 += speedPlatfPlr2
-        # Обработка нижней границы
+            # Обработка нижней границы
         if coordYPlatfPlr2 > winHeight - heightPlatfPlr2:
             coordYPlatfPlr2 = winHeight - heightPlatfPlr2
             
         # Пересечение шарика и платформы
-           #Обработка отскакивания 
-         
         
         # Свободное движение шарика
-    coordXBall += speedXBall
+    coordXBall -= speedXBall
     coordYBall += speedYBall
-            
+    
     if coordYBall + radiusBall > winHeight or coordYBall < radiusBall:
-        speedYBall *= -1        
-
+        speedYBall *= -1
+        
         # Генерация бонусов
  
         # Пересечение бонуса и платформы
 
-
-                    
+        # Условие GameOver'a Игрока 1
+    if coordXBall == radiusBall:
+        gameOverPlr1 = True
+        
+        # Условие GameOver'a Игрока 2
+    if coordXBall + radiusBall == winWidth:
+        gameOverPlr2 = True 
+        
     # Код для рисования
     screen.fill(white)
     
         # Основной режим игры
-        
     # Выведение кол-ва очков, набранных Игроком 1
-    plr1Font = pygame.font.Font(None, 25)
-    textScrPlr1 = plr1Font.render("Счёт 1 Игрока:" + str(scorePlr1), True, red)
+    plr1Font = pygame.font.SysFont("Calibri", 17)
+    textScrPlr1 = plr1Font.render("Счёт 1 Игрока: " + str(scorePlr1), True, red)
     screen.blit(textScrPlr1, [5, 1])
     
     # Выведение кол-ва очков, набранных Игроком 2
-    plr2Font = pygame.font.Font(None, 25)
-    textScrPlr2 = plr2Font.render("Счёт 2 Игрока:" + str(scorePlr2), True, siniy)
-    screen.blit(textScrPlr2, [500, 1])    
+    plr2Font = pygame.font.SysFont("Calibri", 17)
+    textScrPlr2 = plr2Font.render("Счёт 2 Игрока: " + str(scorePlr2), True, siniy)
+    screen.blit(textScrPlr2, [510, 1])    
     
     # Прорисовкаплатформы Игрока 1
     pygame.draw.rect(screen, red, [2, coordYPlatfPlr1, widthPlatfPlr1, heightPlatfPlr1])
     
     # Прорисовка платформы Игрока 2
     pygame.draw.rect(screen, siniy, [winWidth - (widthPlatfPlr1 + 2), coordYPlatfPlr2, widthPlatfPlr2, heightPlatfPlr2])
-    # Прорисовка Шарика
+    
+    # Прорисовка шарика
     pygame.draw.circle(screen, black, [coordXBall, coordYBall], radiusBall)
+    
+    
         # Конец игры
-
+    # Случай проигрыша Игрока 1
+    if gameOverPlr1 == True:
+        screen.fill(white)
+        gameOverFont = pygame.font.SysFont("Calibri", 20)
+        textGmOvPlr1 = gameOverFont.render("Игрок 1 проиграл :( Итоговый счёт Игрока 1: " + str(scorePlr1) + " Итоговый счёт Игрока 2: " + str(scorePlr2), True, black)
+        screen.blit(textGmOvPlr1, [5, 210])
+    # Случай проигрыша Игрока 2    
+    if gameOverPlr2 == True:
+        screen.fill(white)
+        gameOverFont = pygame.font.SysFont("Calibri", 20)
+        textGmOvPlr2 = gameOverFont.render("Игрок 2 проиграл :( Итоговый счёт Игрока 1: " + str(scorePlr1) + " Итоговый счёт Игрока 2: " + str(scorePlr2), True, black)
+        screen.blit(textGmOvPlr2, [5, 210])
+        
     pygame.display.update()      
     clock.tick(200)
 
